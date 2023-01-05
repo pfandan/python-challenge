19a02141318a48f643ed03b9cad7b0481e486c76
Poonam <pfandan@gmail.com>
2023-01-05 01:29:36 -0800

    new file:   PyPoll_challenge.ipynb

---
+++ PyBank/main.py
@@ -0,0 +1,11 @@
+import csv
+import os
+
+month = 0 
+csvpath = os.path.join("..", "Resources" , "budget_data.csv")
+reader_object = csv.reader(csvpath)
+for row in reader_object
+     total_month = month + 1 
+print total_month
+    
+    
